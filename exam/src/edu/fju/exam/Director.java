c79b34c6cdf808f5497caf1d8d0b12728501fa63
IrisAndMimi <my1ris227@outlook.com>
2018-01-16 15:29:25 +0800

    exam

---
+++ exam/src/edu/fju/exam/Director.java
@@ -0,0 +1,9 @@
+package edu.fju.exam;
+
+public class Director extends Manager {
+	public Director(String nam,int money){
+		super(nam,money+5000);
+
+	}
+
+}
---
+++ exam/src/edu/fju/exam/Manager.java
@@ -0,0 +1,10 @@
+package edu.fju.exam;
+
+public class Manager extends Employee {
+	public Manager(String nam,int money){
+		super(nam,money+5000);
+		
+	}
+
+	
+}
